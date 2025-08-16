d70ef7d37be27e3ca2c929b14931a84cd8dcdda7
Aleksander <aleksandera9@gmail.com>
2025-08-16 23:33:04 +0300

    feat: implement csv file compiled in the server

--- src/components/SelectedItems/downloadHandler.ts
+++ src/components/SelectedItems/downloadHandler.ts
@@ -1,31 +1,20 @@
+import { csvCompile } from '../../app/action';
 import type { IGame } from '../../Types/types';
-import { prepareSelectData } from '../../utils/prepareSelectData';
 
-export const downloadHandler = (items: IGame[]) => {
-  const downloadCsv = (filename: string, array: string[][]) => {
-    const csvContent = array
-      .map((row) =>
-        row
-          .map((field) => `"${field.toString().replace(/"/g, '""')}"`)
-          .join(',')
-      )
-      .join('\n');
+const downloadCsv = (filename: string, csvString: string) => {
+  const blob = new Blob([csvString], { type: 'text/csv;charset=utf-8;' });
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement('a');
+  link.href = url;
+  link.download = filename;
+  link.style.display = 'none';
 
-    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
-    const url = URL.createObjectURL(blob);
-    const link = document.createElement('a');
-    link.href = url;
-    link.download = filename;
-    link.style.display = 'none';
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+};
 
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  };
-
-  const exportArray = prepareSelectData(items);
-  downloadCsv(
-    `${exportArray.length - 1 + ''}_game${exportArray.length > 2 ? 's' : ''}.csv`,
-    exportArray
-  );
+export const downloadHandler = async (items: IGame[]) => {
+  const { csvContent, count } = await csvCompile(items);
+  downloadCsv(`${count - 1 + ''}_game${count > 2 ? 's' : ''}.csv`, csvContent);
 };
