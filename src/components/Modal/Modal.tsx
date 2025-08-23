d2d512b0f65e20fee4b7d95c4240afe1c2cb00e7
Aleksander <aleksandera9@gmail.com>
2025-08-23 18:21:38 +0300

    feat: add esc key for close modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import type { JSX } from 'react';
+import { useEffect, type JSX } from 'react';
 import classNames from 'classnames';
 import s from './Modal.module.sass';
 
@@ -8,6 +8,19 @@ type Props = {
 };
 
 export function Modal(props: Props): JSX.Element {
+  const escHandler = (event: KeyboardEvent) => {
+    if (event.code === 'Escape') {
+      props.closeHandler();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('keydown', escHandler);
+    return () => {
+      document.removeEventListener('keydown', escHandler);
+    };
+  }, []);
+
   const modalStyles = classNames(s.modal + ' modal');
 
   const modalClickHandler = () => {
